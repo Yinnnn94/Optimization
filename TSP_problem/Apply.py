164e9033df3a1fb26c7e0b6f5a2ef2e100fa486d
unknown <rungina890904@gmail.com>
2024-06-07 11:35:30 +0800

    Apply program

--- TSP_problem/Apply.py
+++ TSP_problem/Apply.py
@@ -1,12 +1,11 @@
-from Kmeans import KMEANS_with_limitation
+from Kmeans import Kmeans_with_limitation
 import matplotlib.pyplot as plt
-
 from GA import GA
 import pandas as pd
 import time
 
 start_time = time.time()
-data = pd.read_excel(r"C:\Users\user\OneDrive - yuntech.edu.tw\文件\Python Scripts\Simulated_Annealing\TSP_problem\Data.xlsx")
+data = pd.read_excel(r"Data.xlsx")
 Mings = data.iloc[0].values
 data = data.drop(0)
 data_np = data.to_numpy()
@@ -16,22 +15,23 @@ k = 5
 loc = data_np[:,1:]
 
 # Kmeans Program
-kmeans= KMEANS_with_limitation(k, loc)
+kmeans= Kmeans_with_limitation(k, loc)
 m, n = kmeans.get_shape()
 cores = kmeans.kmeans_init_center(m, n)
 data_kmeans = kmeans.kmeans_result(cores, m, n)
-kmeans.plot(data_kmeans, cores,1)
 for i in range(30): 
     new_cores = kmeans.redefine_center(data_kmeans)
     data_kmeans = kmeans.kmeans_result(new_cores, m, n)
 
-kmeans.plot(data_kmeans, new_cores, i)
+kmeans.plot(data_kmeans, i)
 
 # GA program
-k_num = range(0,5)
+k_num = range(0,k)
 path_sum = 0
-for k in k_num: 
-    ga = GA(k,20,10,0.9,0.1) #K = 1, 初始基因解20, 選擇18個, 交配機率. 突變機率
+fig, axs = plt.subplots(2, 3, figsize=(20, 5))
+
+for k,ax in zip(k_num, axs.ravel()): 
+    ga = GA(k,20, 10, 0.9, 0.1) #K = 1, initial 20 gene, choose 10 gene, cross_probability, mutation_probability
     fitness_list = []
     inter = 50
     while inter != 0:
@@ -47,11 +47,13 @@ for k in k_num:
     min_fitness = min(fitness_list)
     path_sum += min_fitness
     min_fitness_index = fitness_list.index(min_fitness)
-    plt.title(f'{k} output')
-    plt.plot(fitness_list)
-    plt.scatter(min_fitness_index,min_fitness, color = 'r')
-    plt.annotate(f'min fitness: {min_fitness}', (min_fitness_index + .05, min_fitness + .05))
-    plt.show()
+    ax.set_title(f'{k} output')
+    ax.plot(fitness_list)
+    ax.scatter(min_fitness_index,min_fitness, color = 'r')
+    ax.annotate(f'min fitness: {min_fitness}', (min_fitness_index + .05, min_fitness + .05))
+# 刪掉最後一個圖片
+axs[1,2].remove()
+plt.show()
 end_time = time.time()
-print(f"所有路徑加總為:{round(path_sum, 3)}")
-print('運行時間:', round((end_time - start_time), 4))
+print(f"Total path:{round(path_sum, 3)}")
+print('Runtime:', round((end_time - start_time), 4))
